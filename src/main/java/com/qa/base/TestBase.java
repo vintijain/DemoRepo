599ae2fc3d332953074391516891aed71c3b7ea6
vintijain <vintijain1@gmail.com>
2019-06-09 18:02:18 +0530

    First Git commit for my selenium code

---
+++ src/main/java/com/qa/base/TestBase.java
@@ -0,0 +1,9 @@
+package com.qa.base;
+
+public class TestBase {	
+	public void testMethod()
+	{
+		System.out.println("Test Base");
+	}
+
+}
---
+++ src/test/java/com/qa/pages/HomePageTest.java
@@ -0,0 +1,13 @@
+package com.qa.pages;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+	
+	@Test
+	public void homePageTest()
+	{
+		System.out.println("Home Page Test");
+	}
+
+}
